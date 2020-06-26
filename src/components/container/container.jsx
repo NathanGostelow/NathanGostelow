50d67a4a98654b24b4921dc0a1f4d4e69db8525f
NathanGostelow <nathan-gostelow-94@hotmail.com>
2020-06-26 19:31:57 +0100

    more design

--- src/components/container/container.jsx
+++ src/components/container/container.jsx
@@ -7,32 +7,30 @@ const Container = () => {
         <div className={containerStyles.container}>
         
             <div className={containerStyles.main}> 
-                    <div className={containerStyles.header}>
-                        <img 
-                            className={containerStyles.nathan}
-                            src={Nathan}
-                            alt="Nathan Gostelow"
-                        />
-                        <div className={containerStyles.headerContent}>
-                            <h1 className={containerStyles.headerName}>
-                                Nathan Gostelow
-                            </h1>
-                            <p className={containerStyles.headerTitle}> Senior Frontend Engineer </p>
-                            <div className={containerStyles.buttonContainer}>
+                <div className={containerStyles.header}>
+                    <img 
+                        className={containerStyles.nathan}
+                        src={Nathan}
+                        alt="Nathan Gostelow"
+                    />
+                    <div className={containerStyles.headerContent}>
+                        <h1 className={containerStyles.headerName}>
+                            Nathan Gostelow
+                        </h1>
+                        <p className={containerStyles.headerTitle}> Senior Frontend Engineer </p>
+                        <p className={containerStyles.description}> 
+                            Nathan is an enthusiastic developer who loves nothing more than to dissect 
+                            complicated problems and provide elegant solutions.
+                        </p>
+                        <div className={containerStyles.buttonContainer}>
                             <a className={containerStyles.button} href="https://github.com/NathanGostelow"> Github </a>
                             <a className={containerStyles.button} href="https://www.linkedin.com/in/nathangostelow/"> LinkedIn</a>
                             <a className={containerStyles.button} href="https://twitter.com/nathan_MG1"> Twitter</a>
+                            <a className={containerStyles.button} href="#"> Read Me...</a>
                         </div>
-                        </div>
-                    </div>
-                    <div className={containerStyles.details}>
-                        <div className={containerStyles.description}> 
-                            Nathan is an enthusiastic developer who loves nothing more than to dissect 
-                            complicated problems and provide elegant solutions.
-                        </div>
-                        <a className={containerStyles.button} href="#"> Find Out More...</a>
                     </div>
                 </div>
+            </div>
         </div>
     )
 }
