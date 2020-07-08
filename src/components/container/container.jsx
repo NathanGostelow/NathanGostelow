33568c28a38272e3933865e645572308383cfdb2
NathanGostelow <nathan-gostelow-94@hotmail.com>
2020-07-08 17:08:49 +0100

    feat: remove dead code and tidy up

--- src/components/container/container.jsx
+++ src/components/container/container.jsx
@@ -3,6 +3,8 @@ import containerStyles from './container.module.css';
 import Nathan from '../../images/NathanGostelow.png';
 const Container = () => {
 
+    const cvPdf = null;
+
     return(
         <div className={containerStyles.main}> 
             <div className={containerStyles.header}>
@@ -16,14 +18,14 @@ const Container = () => {
                         Nathan Gostelow
                     </h1>
                     <p className={containerStyles.headerTitle}> Senior Frontend Engineer </p>
-                    <p className={containerStyles.description}> 
-                        Dissecting complicated problems /<br></br> Providing elegant solutions
-                    </p>
+                    <div className={containerStyles.description}> 
+                        <span className={containerStyles.tagline}>Dissecting complicated problems.</span><span className={containerStyles.tagline}>Providing elegant solutions.</span> 
+                    </div>
                     <div className={containerStyles.buttonContainer}>
                         <a className={containerStyles.button} href="https://github.com/NathanGostelow"> Github </a>
                         <a className={containerStyles.button} href="https://www.linkedin.com/in/nathangostelow/"> LinkedIn</a>
                         <a className={containerStyles.button} href="https://twitter.com/nathan_MG1"> Twitter</a>
-                        <a className={containerStyles.button} href="#"> Read Me...</a>
+                        {cvPdf && <a className={containerStyles.button} href="#"> Read Me...</a>}
                     </div>
                 </div>
             </div>
