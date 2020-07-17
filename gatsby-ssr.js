0783343f9c0d9b21aec5c153e00fc56f45773914
NathanGostelow <nathan-gostelow-94@hotmail.com>
2020-07-17 15:59:18 +0100

    moar fix

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,56 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
+import React from "react";
+import PropTypes from "prop-types";
 
-// You can delete this file if you're not using it
-import "./src/styles/global.css";
+export default class HTML extends React.Component {
+    render() {
+        return (
+            <html {...this.props.htmlAttributes}>
+                <head>
+                    <meta charSet='utf-8' />
+                    <meta httpEquiv='x-ua-compatible' content='ie=edge' />
+                    <meta
+                        name='viewport'
+                        content='width=device-width, initial-scale=1, shrink-to-fit=no'
+                    />
+                    {this.props.headComponents}
+                </head>
+                <body {...this.props.bodyAttributes} className='light'>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                (function() {
+                                    const prefersDarkMode =
+                                        typeof window !== "undefined"
+                                            ? window.matchMedia("(prefers-color-scheme: dark)").matches
+                                            : true;
+
+                                    const setDarkMode = mode =>
+                                        typeof document !== "undefined" && document.documentElement.setAttribute("data-theme", mode);
+
+                                    if (prefersDarkMode) setDarkMode("dark");
+
+                                })();
+                                `,
+                        }}
+                    />
+                    {this.props.preBodyComponents}
+                    <div
+                        key={`body`}
+                        id='___gatsby'
+                        dangerouslySetInnerHTML={{ __html: this.props.body }}
+                    />
+                    {this.props.postBodyComponents}
+                </body>
+            </html>
+        );
+    }
+}
+
+HTML.propTypes = {
+    htmlAttributes: PropTypes.object,
+    headComponents: PropTypes.array,
+    bodyAttributes: PropTypes.object,
+    preBodyComponents: PropTypes.array,
+    body: PropTypes.string,
+    postBodyComponents: PropTypes.array,
+};
