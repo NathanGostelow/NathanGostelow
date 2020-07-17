e3090a1510b7ad40f9fe12fec2bf961dd89f2d82
NathanGostelow <nathan-gostelow-94@hotmail.com>
2020-07-17 13:30:49 +0100

    add a toggle for dark mode

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import layoutStyles from "./layout.module.css";
 
 const Layout = ({ children }) => {
-    return <div className={layoutStyles.layout}>{children}</div>;
+    const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)")
+        .matches;
+    if (prefersDarkMode)
+        document.documentElement.setAttribute("data-theme", "dark");
+
+    useEffect(() => {
+        if (!prefersDarkMode)
+            document.documentElement.setAttribute("data-theme", "light");
+    }, [prefersDarkMode]);
+
+    const toggleTheme = event => {
+        if (event.target.checked) {
+            document.documentElement.setAttribute("data-theme", "dark");
+        } else {
+            document.documentElement.setAttribute("data-theme", "light");
+        }
+    };
+
+    return (
+        <div className={layoutStyles.layout}>
+            <label htmlFor='checkbox' className={layoutStyles.switch}>
+                <input
+                    onClick={toggleTheme}
+                    type='checkbox'
+                    id='checkbox'
+                    defaultChecked={prefersDarkMode}
+                />
+                <div className={layoutStyles.sliderRound}></div>
+            </label>
+            {children}
+        </div>
+    );
 };
 
 export default Layout;
